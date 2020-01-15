cff204a31b8c76e5d4e5e5447d2f50c841ce4327
tihangrai <tihangrai@gmail.com>
2020-01-15 01:48:17 -0500

    added node fetch

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,16 @@
+require('dotenv').config();
 const express = require('express');
-const os = require('os');
+const fetch = require('node-fetch');
 
 const app = express();
 
+
 app.use(express.static('dist'));
-app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+
+app.get('/api/getData', (req, res) => {
+  fetch('https://samples.openweathermap.org/data/2.5/weather?id=2172797&appid=b6907d289e10d714a6e88b30761fae22')
+    .then(response => response.json()
+      .then(data => res.json(data)));
+});
 
 app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
