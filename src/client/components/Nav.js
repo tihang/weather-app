c1e7d01140114ca37444a3851875b192a178c23e
tihangrai <tihangrai@gmail.com>
2020-01-17 16:22:15 -0500

    updated react version to use hooks

---
+++ src/client/components/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/app.scss';
+
+const Nav = () => (
+  <nav>
+    <h3>Logo</h3>
+    <ul>
+      <li>Home</li>
+      <li>Profile</li>
+    </ul>
+  </nav>
+);
+export default Nav;
