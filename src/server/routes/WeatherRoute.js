1525df618278f5c6722118ae144151292bb7ad9c
tihangrai <tihangrai@gmail.com>
2020-01-17 14:32:37 -0500

    refactored weather route api

--- src/server/routes/WeatherRoute.js
+++ src/server/routes/WeatherRoute.js
@@ -4,34 +4,30 @@ const { auth } = require('../middleware/VerifyToken');
 
 const router = express.Router();
 
-
 // Importing CityList schema
 const CityList = require('../models/CityLists');
 
-
-router.get('/getWeatherByName/:name', auth, (req, res) => {
-  const baseUrl = 'https://api.openweathermap.org/data/2.5/weather?q=';
-  const { name } = req.params;
+router.get('/getWeatherById', auth, (req, res) => {
+  const baseUrl = 'https://api.openweathermap.org/data/2.5/weather?id=';
+  const { id } = req.query;
   const appKey = process.env.WEATHER_API_KEY;
 
-  const apiUrl = baseUrl + name + appKey;
+  const apiUrl = baseUrl + id + appKey;
 
   fetch(apiUrl)
     .then(data => data.json())
     .then(data => res.json({ data }));
 });
 
-
-router.get('/getIdByName/:name', async (req, res) => {
-  const { name } = req.params;
-  const data = await CityList.find({ name });
+router.get('/getCityByName', async (req, res) => {
+  const { name } = req.query;
+  const cities = await CityList.find({ name }, ['id', 'name']);
 
   try {
-    res.send(data);
+    res.status(200).send(cities);
   } catch (error) {
     res.status(500).send(error);
   }
 });
-
 
 module.exports = router;
