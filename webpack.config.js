758718707b5feeb19117d1b944213e3e2ae35a1b
tihangrai <tihangrai@gmail.com>
2020-01-20 17:22:31 -0500

    added _redirects file for netlify

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 const outputDirectory = 'dist';
 
@@ -46,6 +47,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: './public/index.html',
       favicon: './public/favicon.ico'
-    })
+    }),
+    new CopyPlugin([{ from: './public/_redirects' }])
   ]
 };
